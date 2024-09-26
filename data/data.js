16642241417b2fcf8ed42a6c544643ea98659ce6
Samuel <sajeronimo@scl.edu.gt>
2024-09-26 12:05:11 -0600

    Se agrega más diseño a proyectos -Se crean mas elementos -Se crea el data y data.js -Dentro de el data.js se agrega la lista de atributos

---
+++ data/data.js
@@ -0,0 +1,11 @@
+let dataProyectos = [
+    {
+        nombre: "Formulario",
+        descripcion: "texto texto texto",
+        githubPage: "https://github.com/samjeronimo/Pryectos--Formulario",
+        github: "https://github.so",
+        stacks: ["js", "css", "html"]
+    },
+];
+
+export { dataProyectos };
