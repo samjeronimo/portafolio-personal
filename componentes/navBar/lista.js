68ca6c8605e5e075e5d1b9ece92eb0c692d47609
Samuel <sajeronimo@scl.edu.gt>
2024-10-03 12:30:06 -0600

    Se termina de haver el panel lateral -Se cre la lista -Se crea la parte de contactar

---
+++ componentes/navBar/lista.js
@@ -0,0 +1,82 @@
+//1
+let divHerra = document.createElement('div');
+divHerra.className = "div-herra";
+
+let miniHerra = document.createElement('div');
+miniHerra.className = "mini-Herra"
+divHerra.appendChild(miniHerra);
+
+let cuadroHerra = document.createElement('div');
+cuadroHerra.className = "cuadro-herra";
+miniHerra.appendChild(cuadroHerra);
+
+let titleHerra = document.createElement('h3');
+titleHerra.innerText = "Nombre Herramienta";
+miniHerra.appendChild(titleHerra);
+
+//2
+let miniHerra2 = document.createElement('div');
+miniHerra2.className = "mini-Herra2";
+divHerra.appendChild(miniHerra2);
+
+let cuadroHerra2 = document.createElement('div');
+cuadroHerra2.className = "cuadro-herra2";
+miniHerra2.appendChild(cuadroHerra2);
+
+let titleHerra2 = document.createElement('h3');
+titleHerra2.innerText = "Nombre Herramienta";
+miniHerra2.appendChild(titleHerra2);
+
+//3
+let miniHerra3 = document.createElement('div');
+miniHerra3.className = "mini-Herra3";
+divHerra.appendChild(miniHerra3);
+
+let cuadroHerra3 = document.createElement('div');
+cuadroHerra3.className = "cuadro-herra3";
+miniHerra3.appendChild(cuadroHerra3);
+
+let titleHerra3 = document.createElement('h3');
+titleHerra3.innerText = "Nombre Herramienta";
+miniHerra3.appendChild(titleHerra3);
+
+//4
+let miniHerra4 = document.createElement('div');
+miniHerra4.className = "mini-Herra4";
+divHerra.appendChild(miniHerra4);
+
+let cuadroHerra4 = document.createElement('div');
+cuadroHerra4.className = "cuadro-herra4";
+miniHerra4.appendChild(cuadroHerra4);
+
+let titleHerra4 = document.createElement('h3');
+titleHerra4.innerText = "Nombre Herramienta";
+miniHerra4.appendChild(titleHerra4);
+
+//5
+let miniHerra5 = document.createElement('div');
+miniHerra5.className = "mini-Herra5";
+divHerra.appendChild(miniHerra5);
+
+let cuadroHerra5 = document.createElement('div');
+cuadroHerra5.className = "cuadro-herra5";
+miniHerra5.appendChild(cuadroHerra5);
+
+let titleHerra5 = document.createElement('h3');
+titleHerra5.innerText = "Nombre Herramienta";
+miniHerra5.appendChild(titleHerra3);
+
+//6
+let miniHerra6 = document.createElement('div');
+miniHerra6.className = "mini-Herra6";
+divHerra.appendChild(miniHerra6);
+
+let cuadroHerra6 = document.createElement('div');
+cuadroHerra6.className = "cuadro-herra6";
+miniHerra6.appendChild(cuadroHerra6);
+
+let titleHerra6 = document.createElement('h3');
+titleHerra6.innerText = "Nombre Herramienta";
+miniHerra6.appendChild(titleHerra6);
+
+export { divHerra };
